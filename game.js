18073f0f7bed5d38ca363a4c6e3a69ce3c57dae1
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2022-08-01 15:09:45 -0400

    got rid of unnecessary scenes

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ var gameSettings = {
 
 }
 var config ={
+width:256,
+height:272,
+backgroundColor: 0x000000,
+scene:[Scene1,Scene2]
 
 }
 
--- scene1.js
+++ scene1.js
@@ -6,11 +6,6 @@ class Scene1 extends Phaser.Scene {
   preload() {
     this.load.image("background", "assets/gameboard.png");
 
-    this.load("red", "assets/red.png");
-    this.load("yellow", "assets/yellow.png");
-    this.load("restartbtn", "assets/restart.png");
-    this.load("easybtn", "assets/easybtn.png");
-    this.load("playbtn", "assets/playbtn.png");
-    this.load("hardbtn", "assets/hardbtn.png");
+  
   }
 }
